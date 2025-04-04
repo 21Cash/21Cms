69139e56bfc1400a95a7a4b6d01c7afc53dc0a3b
21Cash <21ThousandProof@gmail.com>
2025-04-04 22:10:10 +0530

    When trying to verfiy register credentails, a new browser instance is used

--- server/src/lib/scraping/try-login.ts
+++ server/src/lib/scraping/try-login.ts
@@ -1,6 +1,9 @@
-import { Page } from "puppeteer";
-import { browser } from "../..";
-import { cmsUrl } from "../../constants";
+import puppeteer from "puppeteer";
+import {
+  chromeExecutablePath,
+  cmsUrl,
+  useHeadlessBrowser,
+} from "../../constants";
 
 interface UserInfo {
   userId: string;
@@ -8,7 +11,6 @@ interface UserInfo {
   password: string;
 }
 
-// TODO: Use another browser to validate credentials by logging in so that when full-update-infos are being triggered, this login doesn't interfere
 const tryLogin = async (
   userId: string,
   password: string
@@ -16,32 +18,47 @@ const tryLogin = async (
   console.log(
     `Trying to log in with userId: ${userId} and password: ${password} on cmsUrl: ${cmsUrl}`
   );
-  if (!browser) {
-    console.log(`Browser is not initialized`);
-    return;
-  }
 
-  const page = await browser.newPage();
+  // Launch a new browser instance so that it doesn't interfere with full info updates.
+  const browserInstance = await puppeteer.launch({
+    executablePath: chromeExecutablePath,
+    headless: useHeadlessBrowser,
+  });
 
-  await page.goto(cmsUrl, { waitUntil: "networkidle2" });
-
-  await page.type("#txt_login", userId);
-  await page.type("#txt_pswd", password);
-  await page.click("#btnsubmit");
+  const page = await browserInstance.newPage();
 
   try {
-    await page.waitForSelector("#lblname", { timeout: 5000 });
+    await page.goto(cmsUrl, { waitUntil: "networkidle2" });
+
+    await page.type("#txt_login", userId);
+    await page.type("#txt_pswd", password);
+    await page.click("#btnsubmit");
+
+    try {
+      await page.waitForSelector("#lblname", { timeout: 3000 });
+    } catch (err) {
+      throw new Error(
+        "Failed to login with given credentials. Recheck Credentails"
+      );
+    }
+
     const username = await page.$eval("#lblname", (el) =>
       (el as HTMLElement).innerText.trim()
     );
+
+    if (!username) {
+      throw new Error(
+        "Failed to login with the given credentials, or page load timed out"
+      );
+    }
+
     return { userId, password, username };
-  } catch (error) {
-    throw new Error(
-      "Failed to Login with the given credentials, recheck the credentials provided."
-    );
   } finally {
     if (page) {
       await page.close();
+    }
+    if (browserInstance) {
+      await browserInstance.close();
     }
   }
 };
